580e35110332174f89774f63f52f72cd548d8b37
Justin Maronde <myemailisjustin@me.com>
2024-08-28 07:47:46 -0400

    Initial Commit

---
+++ HelloValidation/HelloValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloValidationApp.swift
+//  HelloValidation
+//
+//  Created by Justin Maronde on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
