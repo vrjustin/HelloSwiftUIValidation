5058ced19649d5fb75c833b2cb5eb0b846cf5aae
Justin Maronde <myemailisjustin@me.com>
2024-08-28 08:04:03 -0400

    simple form that uses validation bool to show/hide the button enabled.

--- HelloValidation/LoginView.swift
+++ HelloValidation/LoginView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var isFormValid: Bool {
+        return !email.isEmpty && !password.isEmpty && email.isValidEmail
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Email", text: $email)
+                .textInputAutocapitalization(.never)
+            SecureField("Password", text: $password)
+            Button("Login") {
+                
+            }.disabled(!isFormValid)
+        }
     }
 }
 
